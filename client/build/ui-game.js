fc6bffb839230dff927f08cf9bc38477bec7613e
nikhar12 <40731967+nikhar12@users.noreply.github.com>
2024-03-28 08:58:44 +0530

    gamefeaturesupdated

--- client/build/ui-game.js
+++ client/build/ui-game.js
@@ -1,12 +1,98 @@
 $(document).ready(function () {
-  //lis clicked for username
-
   //clicking on ttt boxes
-  $(".gbox").click(function () {
-    let boxnumber = $(this).attr("id");
-    //alert(boxnumber);
 
-    //block that box -disable click
-    $(this).off("click");
+  $(".gbox").click(function () {
+    if (usertype != "spec") {
+      if (username == turn) {
+        let boxnumber = $(this).attr("id");
+        console.log(boxnumber);
+        if (blockBox.find((p) => p == boxnumber) == boxnumber) return;
+        blockBox.push(boxnumber);
+        $(this).html(insertImage);
+        //block that box -disable click
+        // $(this).prop("disabled", true);
+
+        let ijvalues = String(kvpair[boxnumber]);
+        let arr = ijvalues.split("-");
+        let i = arr[0];
+        let j = arr[1];
+
+        //if valid turn
+        if (gameBoard[i][j] == -1) {
+          gameBoard[i][j] = k;
+          let data = {
+            from: username,
+            to: opponent,
+            boxnumber: boxnumber,
+            k: k,
+          };
+          socket.emit("userturn", data);
+        }
+      }
+    }
   });
 });
+
+let gameBoard;
+initGame();
+let gameObjectt;
+let insertImage;
+let turn, k;
+let kvpair = {
+  1: "0-0",
+  2: "0-1",
+  3: "0-2",
+  4: "1-0",
+  5: "1-1",
+  6: "1-2",
+  7: "2-0",
+  8: "2-1",
+  9: "2-2",
+};
+function initGame() {
+  gameBoard = [
+    [-1, -1, -1],
+    [-1, -1, -1],
+    [-1, -1, -1],
+  ];
+  blockBox = [];
+}
+
+function player_game(data) {
+  $(".gbox").removeAttr("disabled");
+  gameObjectt = data;
+  if (data.x == username) {
+    insertImage = "<img src='/images/ttt-cross.png'/>";
+    k = 1;
+  }
+
+  if (data.o == username) {
+    insertImage = "<img src='/images/ttt-circle.png'/>";
+    k = 0;
+  }
+
+  if (data.firstchance != username) {
+    //$(".gbox").off("click");
+  } else {
+    $(".gbox").removeAttr("disabled");
+  }
+  turn = data.firstchance;
+}
+
+function switchturn(turn, m) {
+  if (m == 0) {
+    $("#div-circle").css("background-color", "yellow");
+    $("#div-cross").css("background-color", "white");
+  } else {
+    $("#div-cross").css("background-color", "yellow");
+    $("#div-circle").css("background-color", "white");
+  }
+  if (turn == username) {
+    turn = opponent;
+  } else {
+    turn = username;
+  }
+}
+function spectator_game(data) {
+  //$(".gbox").attr("disabled", "disabled");
+}
--- index.js
+++ index.js
@@ -22,6 +22,33 @@ app.use(express.static("client/build"));
 app.use(express.json());
 
 let arrUsernameSocketid = [];
+let matchLogger = [];
+let gameStartObject;
+let currentMatchID;
+let que = [];
+let gameBoard = [
+  [-1, -1, -1],
+  [-1, -1, -1],
+  [-1, -1, -1],
+];
+function initGameboard() {
+  gameBoard = [
+    [-1, -1, -1],
+    [-1, -1, -1],
+    [-1, -1, -1],
+  ];
+}
+let kvpair = {
+  1: "0-0",
+  2: "0-1",
+  3: "0-2",
+  4: "1-0",
+  5: "1-1",
+  6: "1-2",
+  7: "2-0",
+  8: "2-1",
+  9: "2-2",
+};
 
 io.on("connection", (socket) => {
   console.log(`User connected: ${socket.id}`);
@@ -44,6 +71,10 @@ io.on("connection", (socket) => {
       arrusernames.push(data);
     });
     io.to("main-chat-group").emit("userlist", arrusernames);
+    let m = matchLogger.find((p) => p.onGoing == 1);
+    if (m != null) {
+      io.to(socket.id).emit("game-started", m);
+    }
   });
 
   socket.on("messagefromclient", (data) => {
@@ -55,27 +86,124 @@ io.on("connection", (socket) => {
   });
 
   socket.on("game_request", (data) => {
-    //console.log(data);
+    console.log(data);
     //username to send request to
     let tosocket = arrUsernameSocketid.find((o) => o.username == data.to);
     if (tosocket != null) {
       io.to(tosocket.socketid).emit("incoming_request", data.from);
     }
   });
-  let matchLogger = [];
+
   socket.on("requests_response", (data) => {
-    let gameStartObject = {
+    currentMatchID = "id" + Math.random().toString(16).slice(2);
+    gameStartObject = {
       from: data.from,
       to: data.to,
       x: data.from,
       o: data.to,
       matchno: matchLogger.length + 1,
+      onGoing: 1,
+      currentMatchID: currentMatchID,
+      likes: 0,
+      firstchance: data.from,
+      que: [],
+      turncounter: 0,
+      votecounter: 0,
     };
     console.log(gameStartObject);
     io.to("main-chat-group").emit("game-started", gameStartObject);
     matchLogger.push(gameStartObject);
-    socket.off("game_request");
   });
+  let votecounter = 0;
+  socket.on("game_controls", (data) => {
+    if (data.type == "abandon") {
+      let m = matchLogger.find((p) => p.currentMatchID == data.currentMatchID);
+      if (m.from == data.player || m.to == data.player) {
+        matchLogger.find(
+          (p) => p.currentMatchID == data.currentMatchID
+        ).onGoing = 0;
+
+        let gameendobj = {
+          player: data.player,
+          gameendby: "abandoning",
+        };
+        io.to("main-chat-group").emit("game-abandoned", gameendobj);
+      }
+    } else if (data.type == "votetostop") {
+      console.log("votetostop by:" + data.player);
+      if (arrUsernameSocketid.length > 2) {
+        matchLogger.find((p) => p.currentMatchID == data.currentMatchID)
+          .votecounter++;
+        //console.log("votetostop by:" + data.player + " votec:" + votecounter);
+        if (
+          arrUsernameSocketid.length - 2 ==
+          matchLogger.find((p) => p.currentMatchID == data.currentMatchID)
+            .votecounter
+        ) {
+          //game end by vote
+          matchLogger.find(
+            (p) => p.currentMatchID == data.currentMatchID
+          ).onGoing = 0;
+          let gameendobj = {
+            player: "",
+            gameendby: "voting",
+          };
+          console.log("gameendobj :" + gameendobj);
+          io.to("main-chat-group").emit("game-abandoned", gameendobj);
+        }
+      }
+    } else if (data.type == "like") {
+      matchLogger.find((p) => p.currentMatchID == data.currentMatchID).likes++;
+    }
+  });
+  let turncounter = 0;
+  let gameobject = {};
+
+  socket.on("userturn", (data) => {
+    //turncounter++;
+    let d = { boxnumber: data.boxnumber, k: data.k };
+    que.push(d);
+    matchLogger.find((p) => p.onGoing == 1).que = que;
+
+    let ijvalues = String(kvpair[data.boxnumber]);
+    let arr = ijvalues.split("-");
+    let i = arr[0];
+    let j = arr[1];
+
+    //if valid turn
+    if (gameBoard[i][j] == -1) {
+      gameBoard[i][j] = data.k;
+      matchLogger.find((p) => p.onGoing == 1).turncounter++;
+    }
+
+    // console.log(turncounter + " gb:" + gameBoard);
+    gameobject.status = 0;
+
+    if (matchLogger.find((p) => p.onGoing == 1).turncounter >= 5) {
+      gameobject = gameWinnerDetector(gameBoard);
+
+      console.log(
+        "gameobject status " +
+          gameobject.status +
+          " code " +
+          gameobject.code +
+          " wonby " +
+          gameobject.wonby
+      );
+      console.log(matchLogger.find((p) => p.onGoing == 1));
+      if (gameobject.status == 1) {
+        matchLogger.find((p) => p.onGoing == 1).que = [];
+        matchLogger.find((p) => p.onGoing == 1).turncounter = 0;
+        matchLogger.find((p) => p.onGoing == 1).votecounter = 0;
+        matchLogger.find((p) => p.onGoing == 1).onGoing = 0;
+        initGameboard();
+      }
+    }
+    data.gamewinobj = gameobject;
+
+    io.to("main-chat-group").emit("userturn-broadcast", data);
+  });
+
   socket.on("disconnect", async () => {
     console.log(`${socket.id} diconnectd`);
 
@@ -95,3 +223,107 @@ io.on("connection", (socket) => {
     io.to("main-chat-group").emit("userlist", arrusernames);
   });
 });
+
+const gameWinnerDetector = (gameb) => {
+  let gameobject = { status: 0 };
+  //gameb is an 2d array
+  let j = 0;
+  let k = 0;
+
+  outerloop: for (let i = 0; i < 3; i++) {
+    //if(gameb[i][j]!=-1|| gameb[k][i]!=-1)
+    //{
+    let a = gameb[i][j];
+    let b = gameb[k][i];
+
+    j++;
+    k++;
+    let row = true;
+    let column = true;
+    innerloop: for (; j < 3; ) {
+      if (a != -1) {
+        //console.log("i:"+i+" j:"+j+" a:"+a);
+        if (gameb[i][j] == a) {
+          j++;
+        } else {
+          a = gameb[i][j];
+          j++;
+          row = false;
+          //break innerloop;
+        }
+      } else {
+        j++;
+        row = false;
+      }
+      //console.log("k:"+k+" i:"+i+" b:"+b);
+      if (b != -1) {
+        //coulm
+        if (gameb[k][i] == b) {
+          k++;
+        } else {
+          b = gameb[k][i];
+          k++;
+          column = false;
+        }
+      } else {
+        column = false;
+      }
+    }
+
+    j = 0;
+    k = 0;
+
+    if (row) {
+      //row - a won
+      //console.log("R"+i+" won by:"+a);
+      gameobject.status = 1;
+      gameobject.code = "R" + i;
+      gameobject.wonby = a;
+      //break outerloop;
+    }
+    if (column) {
+      // some column b won
+
+      //console.log("C"+i+" won by:"+b);
+      gameobject.status = 1;
+      gameobject.code = "C" + i;
+      gameobject.wonby = b;
+      //break outerloop;
+    }
+
+    //}
+  }
+  let down = false;
+  let up = false;
+  let c;
+  if (
+    gameb[0][0] == gameb[1][1] &&
+    gameb[1][1] == gameb[2][2] &&
+    gameb[2][2] != -1
+  ) {
+    down = true;
+    c = gameb[2][2];
+  }
+  if (
+    gameb[2][0] == gameb[1][1] &&
+    gameb[1][1] == gameb[0][2] &&
+    gameb[0][2] != -1
+  ) {
+    up = true;
+    c = gameb[0][2];
+  }
+  if (down) {
+    //console.log("D");
+    gameobject.status = 1;
+    gameobject.code = "D";
+    gameobject.wonby = c;
+  }
+  if (up) {
+    //console.log("U");
+    gameobject.status = 1;
+    gameobject.code = "U";
+    gameobject.wonby = c;
+  }
+  console.log(gameobject.code);
+  return gameobject;
+};
